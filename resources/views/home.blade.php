be6a0d9515ec69367ce83d41e8ce93a49ce27ef3
Alexi Lallas Ribeiro Pereira <alexii2005@hotmail.com>
2019-01-11 13:43:43 -0200

    Add files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,9 +17,42 @@
      Main content -->
     <section class="content container-fluid">
 
-      <!--------------------------
-        | Your Page Content Here |
-        -------------------------->
+     <div class="container-fluid">
+         <div class="row">
+             <div class="alinha">
+                <div class="col-xs-6 col-sm-3 col-md-2">
+                 <a href="" class="btn btn-default">
+                   <div class="row">
+                     <div class="col-xs-12 text-center">
+                       <i class="fa fa-user fa-4x"></i>
+                       <!--<span class="badge badge-pill badge-info fa-1x">15</span>-->
+                     </div>
+                     <div class="col-xs-12 text-center">
+                       <p>Cadastrar</p>
+                       
+                     </div>
+                   </div>
+                 </a>
+               </div>
+
+               <div class="col-xs-6 col-sm-3 col-md-2">
+                 <a href="" class="btn btn-default">
+                   <div class="row">
+                     <div class="col-xs-12 text-center">
+                       <i class="fa fa-edit fa-4x"></i>
+                       <!--<span class="badge badge-pill badge-info fa-1x">15</span>-->
+                     </div>
+                     <div class="col-xs-12 text-center">
+                       <p>Editar</p>
+                       
+                     </div>
+                   </div>
+                 </a>
+                 </div>
+             </div>
+         </div>
+
+     </div>
 
     </section>
     <!-- /.content -->
