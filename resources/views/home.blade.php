494be876bf9cf6d6f80b71290f043aa24869a891
Alexi Lallas Ribeiro Pereira <alexii2005@hotmail.com>
2019-01-13 12:03:21 -0300

    Arquivos adicionados
    Template e models/migrations modificados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,42 +17,9 @@
      Main content -->
     <section class="content container-fluid">
 
-     <div class="container-fluid">
-         <div class="row">
-             <div class="alinha">
-                <div class="col-xs-6 col-sm-3 col-md-2">
-                 <a href="" class="btn btn-default">
-                   <div class="row">
-                     <div class="col-xs-12 text-center">
-                       <i class="fa fa-user fa-4x"></i>
-                       <!--<span class="badge badge-pill badge-info fa-1x">15</span>-->
-                     </div>
-                     <div class="col-xs-12 text-center">
-                       <p>Cadastrar</p>
-                       
-                     </div>
-                   </div>
-                 </a>
-               </div>
-
-               <div class="col-xs-6 col-sm-3 col-md-2">
-                 <a href="" class="btn btn-default">
-                   <div class="row">
-                     <div class="col-xs-12 text-center">
-                       <i class="fa fa-edit fa-4x"></i>
-                       <!--<span class="badge badge-pill badge-info fa-1x">15</span>-->
-                     </div>
-                     <div class="col-xs-12 text-center">
-                       <p>Editar</p>
-                       
-                     </div>
-                   </div>
-                 </a>
-                 </div>
-             </div>
-         </div>
-
-     </div>
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
 
     </section>
     <!-- /.content -->
