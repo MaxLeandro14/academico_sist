471ef28c7f676a45f3d335bb6c4b20e2e82f37df
MaxLeandro14 <maxleandro14>
2019-01-13 18:13:23 -0200

    Arquivos AdminLTE adicionados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,61 +1,11 @@
-@include('layouts.header')
+@extends('adminlte::page')
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) 
-    <section class="content-header">
-      <h1>
-        Page Header
-        <small>Optional description</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
-      </ol>
-    </section>
+@section('title', 'AdminLTE')
 
-     Main content -->
-    <section class="content container-fluid">
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
-     <div class="container-fluid">
-         <div class="row">
-             <div class="alinha">
-                <div class="col-xs-6 col-sm-3 col-md-2">
-                 <a href="" class="btn btn-default">
-                   <div class="row">
-                     <div class="col-xs-12 text-center">
-                       <i class="fa fa-user fa-4x"></i>
-                       <!--<span class="badge badge-pill badge-info fa-1x">15</span>-->
-                     </div>
-                     <div class="col-xs-12 text-center">
-                       <p>Cadastrar</p>
-                       
-                     </div>
-                   </div>
-                 </a>
-               </div>
-
-               <div class="col-xs-6 col-sm-3 col-md-2">
-                 <a href="" class="btn btn-default">
-                   <div class="row">
-                     <div class="col-xs-12 text-center">
-                       <i class="fa fa-edit fa-4x"></i>
-                       <!--<span class="badge badge-pill badge-info fa-1x">15</span>-->
-                     </div>
-                     <div class="col-xs-12 text-center">
-                       <p>Editar</p>
-                       
-                     </div>
-                   </div>
-                 </a>
-                 </div>
-             </div>
-         </div>
-
-     </div>
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  @include('layouts.footer')
+@section('content')
+    <p>You are logged in!</p>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+$this->group(['middleware'=> ['auth'], 'namespace'=>'Painel'], function(){
+    Route::get('/painel', 'PainelController@index')->name('painel.home');
 });
+Route::get('/', 'Site\SiteController@index');
+
+Auth::routes();
+
