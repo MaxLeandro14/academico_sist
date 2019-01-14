779f32a61abd0e7a56bdd54878b74912495092a9
Alexi Lallas Ribeiro Pereira <alexii2005@hotmail.com>
2019-01-14 17:25:53 -0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,26 @@
 <?php
 $this->group(['middleware'=> ['auth'], 'namespace'=>'Painel'], function(){
+
+	Route::get('/', 'PainelController@index')->name('painel.home');
+    
     Route::get('/painel', 'PainelController@index')->name('painel.home');
+
+    Route::get('/cadastrar_turma', function () {
+        return view('painel/cadastrar/turma');
+    })->name('cadastrar_turma');
+
+    Route::get('/cadastrar_aluno', function () {
+        return view('painel/cadastrar/aluno');
+    })->name('cadastrar_aluno');
+
+    Route::get('/cadastrar_professor', function () {
+        return view('painel/cadastrar/professor');
+    })->name('cadastrar_professor');
+
+    Route::get('/relacionar', function () {
+        return view('painel/relacionar/index');
+    })->name('relacionar');
+
 });
-Route::get('/', 'HomeController@index');
-
-Route::get('/cadastrar_turma', function () {
-    return view('painel/cadastrar/turma');
-})->name('cadastrar_turma');
-
-Route::get('/cadastrar_aluno', function () {
-    return view('painel/cadastrar/aluno');
-})->name('cadastrar_aluno');
 
 Auth::routes();
