597b6d46a75de9a9b8dc94a49d6138c4a614dde5
Alexi Lallas Ribeiro Pereira <alexii2005@hotmail.com>
2019-01-17 18:37:50 -0300

    Alterações

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTbableSeeder::class);
+         $this->call(UsersTableSeeder::class);
          $this->call(DisciplinaTableSeeder::class);         
          $this->call(DisciplinaNivelTableSeeder::class);
          $this->call(ProfessorTableSeeder::class);
